30894674c269a3bd41a6260aceea2bb91e3bafdd
Chris Cannam <cannam@all-day-breakfast.com>
2014-10-17 17:25:31 +0100

    Archive script

---
+++ deploy/src/archive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tag=`hg tags | grep '^vect_v' | head -1 | awk '{ print $1; }'`
+
+v=`echo "$tag" |sed 's/vect_v//'`
+
+echo "Packaging up version $v from tag $tag..."
+
+hg archive -r"$tag" --subrepos --exclude sv-dependency-builds /tmp/sonic-vector-"$v".tar.gz
+
